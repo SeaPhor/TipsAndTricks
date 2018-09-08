f21fd84e271cbcb62b9538b6d7d112082595f3a9
SeaPhor <seaphor@woodbeeco.com>
2018-09-08 10:46:40 -0500

    cleanup structure of useful

--- Useful_Tips/useful-tips.sh
+++ Useful_Tips/useful-tips.sh
@@ -1,5 +1,42 @@
 #!/usr/bin/env bash
 #
+##    useful-tips
+##    Author       - Shawn Miller
+##    Date         - 2018 02 15
+##    Colaborators - 
+#
+#####################################################################
+#####                   GNU/GPL Info
+#####################################################################
+#
+gpl_info () {
+    cat <<EOT
+${LTCYN}
+####c4#############################################################################
+###                                                                             ###
+##                      GNU/GPL Info                                             ##
+##        useful-tips.sh ver-0.0.0-01 - Colaborative begun 15 February 2018	 ##
+##      Released under GPL v2.0, See www.gnu.org for full license info           ##
+##      Copyright (C) 2015  Shawn Miller, The Wood-Bee Company			 ##
+##              EMAIL- shawn@woodbeeco.com                                       ##
+##                   - seaphor@woodbeeco.com                                     ##
+##  This program is free software; you can redistribute it and/or modify         ##
+##    it under the terms of the GNU General Public License as published by       ##
+##    the Free Software Foundation; either version 2 of the License, or          ##
+##    (at your option) any later version.                                        ##
+##                                                                               ##
+##    This program is distributed in the hope that it will be useful,            ##
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of             ##
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              ##
+##    GNU General Public License for more details.                               ##
+##                                                                               ##
+##    See this complete License at:                                              ##
+##    https://github.com/SeaPhor/SeaPhor-Scripts/blob/master/LICENSE		 ##
+###                                                                             ###
+####w#################################b######################################c#####${RESET}
+EOT
+}
+#
 ###########################################
 ### Define Variables
 ###########################################
